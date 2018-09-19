03aa0cda8d037adb02a24f01878e80535d7528df
ravi2krishna <ravi2krishna@gmail.com>
2018-09-19 03:22:53 +0000

    changes

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -8,6 +8,6 @@ package com.ravi.p1;
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins 3.0" );
+        System.out.println( "Hello Web Hook Invoked" );
     }
 }
