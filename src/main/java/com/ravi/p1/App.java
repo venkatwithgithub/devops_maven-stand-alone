18f79e468594e8cd56f9f6807bca9cb4437a10c5
ravi2krishna <ravi2krishna@gmail.com>
2019-06-29 10:17:33 +0530

    new feature 2

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -8,6 +8,6 @@ package com.ravi.p1;
 {
     public static void main( String[] args )
     {
-        System.out.println( "Webhook Trigerred" );
+        System.out.println( "Developer added feature 2" );
     }
 }
