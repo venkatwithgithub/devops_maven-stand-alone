4cb3937ec5e25e8345f0f5ab555640fbe180d022
venkatwithgithub <38827364+venkatwithgithub@users.noreply.github.com>
2019-09-04 21:53:22 +0530

    Update App.java

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -6,7 +6,7 @@ package com.ravi.p1;
  */
  public class App  
 {
-    public stati void main( String[] args )
+    public static void main( String[] args )
     {
         System.out.println( "Github Webhook Invoked" );
     }
