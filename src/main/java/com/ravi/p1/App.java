701a249d1210b5f3d4895ea332ea7186756e604a
venkatwithgithub <38827364+venkatwithgithub@users.noreply.github.com>
2019-09-04 21:51:59 +0530

    error

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -6,7 +6,7 @@ package com.ravi.p1;
  */
  public class App  
 {
-    public static void main( String[] args )
+    public stati void main( String[] args )
     {
         System.out.println( "Github Webhook Invoked" );
     }
